2c8bcc08165396d13897d0eba75bb3d0acfe6a13
Joaquin <jtornello2019@hotmail.com>
2021-07-22 11:30:56 -0300

    Now the age is set dynamically

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
-function CopyMail()
-{
-    var copyText = document.getElementById("email");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
+function CopyMail() {
+  var copyText = document.getElementById('email');
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand('copy');
 }
+
+const ageTextElement = document.getElementById('ageText');
+
+const getCurrentAge = () => {
+  const date = new Date();
+
+  return date.getFullYear() - 2001;
+};
+
+ageTextElement.innerText = `${getCurrentAge()} years`;
